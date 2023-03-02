29e77cf7801b058c212cb2ec4766978c466216b5
zakariayazdani-kiwi <zakaria.yazdani@kiwitech.com>
2023-03-02 19:56:37 +0530

    first commit

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react'
+import classes from './Header.module.css'
+import mealImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+const Header =(props)=>{
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                <h1>Zakaria Cafe</h1>
+               <HeaderCartButton />
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealImage} alt='Table with Foods' />
+            </div>
+        </Fragment>
+    )
+}
+export default Header;
